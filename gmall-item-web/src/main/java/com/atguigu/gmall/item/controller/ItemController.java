e71878a894ecbbfdea5315b691ca28f34c2bd391
Administrator <987900544@qq.com>
2020-11-04 16:35:19 +0800

    search

--- gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
@@ -1,8 +1,10 @@
 package com.atguigu.gmall.item.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.bean.PmsProductSaleAttr;
 import com.atguigu.gmall.bean.PmsSkuInfo;
+import com.atguigu.gmall.bean.PmsSkuSaleAttrValue;
 import com.atguigu.gmall.service.SkuService;
 import com.atguigu.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ItemController {
@@ -43,6 +46,25 @@ public class ItemController {
         List<PmsProductSaleAttr> pmsProductSaleAttrList = spuService.spuSaleAttrListCheckBySku(pmsSkuInfo.getProductId(),pmsSkuInfo.getId());
         //销售属性列表
         modelMap.put("spuSaleAttrListCheckBySku",pmsProductSaleAttrList);
+
+        //hashmap
+        Map<String,String> skuSaleAttrHash = new HashMap<>();
+        List<PmsSkuInfo> pmsSkuInfos = skuService.getSkuSaleAttrValueListBySpu(pmsSkuInfo.getProductId());
+        for (PmsSkuInfo skuInfo : pmsSkuInfos) {
+            StringBuilder k = new StringBuilder();
+            String v = skuInfo.getId();
+
+            List<PmsSkuSaleAttrValue> skuSaleAttrValueList = skuInfo.getSkuSaleAttrValueList();
+            for (PmsSkuSaleAttrValue pmsSkuSaleAttrValue : skuSaleAttrValueList) {
+                k.append(pmsSkuSaleAttrValue.getSaleAttrValueId()).append("|");
+            }
+
+            skuSaleAttrHash.put(k.toString(),v);
+        }
+
+        String skuSaleAttrHashJsonStr = JSON.toJSONString(skuSaleAttrHash);
+        modelMap.put("skuSaleAttrHashJsonStr",skuSaleAttrHashJsonStr);
+
         return "item";
     }
 
